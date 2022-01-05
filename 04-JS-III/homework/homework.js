d9ac7e02e120289146b36d649f22bf0d5b7db378
nanin <let.aleph@gmail.com>
2022-01-05 10:25:50 -0300

    04 - JavaScript     - III - > working on

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,155 +1,213 @@
 // No cambies los nombres de las funciones.
 
-function devolverPrimerElemento(array) {
-  // Devuelve el primer elemento de un  array (pasado por parametro)
-  // Tu código:
+function devolverPrimerElemento( array ) {
+  // Devuelve el primer elemento de un array (pasado por parametro)
+  
+  //
+  // the code :
+  //
+
 }
 
 
-function devolverUltimoElemento(array) {
+function devolverUltimoElemento( array ) {
   // Devuelve el último elemento de un array
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function obtenerLargoDelArray(array) {
+function obtenerLargoDelArray( array ) {
   // Devuelve el largo de un array
-  // Tu código:
+
+  //
+  // the code :
+  //
 }
 
 
-function incrementarPorUno(array) {
+function incrementarPorUno( array ) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function agregarItemAlFinalDelArray(array, elemento) {
+function agregarItemAlFinalDelArray( array, elemento ) {
   // Añade el "elemento" al final del array
   // y devuelve el array
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function agregarItemAlComienzoDelArray(array, elemento) {
+function agregarItemAlComienzoDelArray( array, elemento ) {
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function dePalabrasAFrase(palabras) {
+function dePalabrasAFrase( palabras ) {
   // "palabras" es un array de strings/cadenas
   // Devuelve un string donde todas las palabras estén concatenadas
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function arrayContiene(array, elemento) {
+function arrayContiene( array, elemento ) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function agregarNumeros(numeros) {
+function agregarNumeros( numeros ) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function promedioResultadosTest(resultadosTest) {
+function promedioResultadosTest( resultadosTest ) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function numeroMasGrande(numeros) {
+function numeroMasGrande( numeros ) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
-  // Escribe tu código aquí:
+  
+  //
+  // the code :
+  //
 }
 
 
-function cuentoElementos(arreglo){
+function cuentoElementos( arreglo ){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
-  //Escribe tu código aquí
+  
+  //
+  // the code :
+  //
 
 }
 
 
-function diaDeLaSemana(numeroDeDia) {
+function diaDeLaSemana( numeroDeDia ) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
   
+  //
+  // the code :
+  //
 } 
 
 
-function empiezaConNueve(n) {
+function empiezaConNueve( n ) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
-  //Escribe tu código aquí
   
+  //
+  // the code :
+  //
 }
 
 
-function todosIguales(arreglo) {
+function todosIguales( arreglo ) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
   
+  //
+  // the code :
+  //
 } 
 
 
-function mesesDelAño(array) {
+function mesesDelAño( array ) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function mayorACien(array) {
+function mayorACien( array ) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function breakStatement(numero) {
+function breakStatement( numero ) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array. 
   //Devolver el array
   //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
-  // Tu código:
+  
+  //
+  // the code :
+  //
 }
 
 
-function continueStatement(numero) {
+function continueStatement( numero ) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
-  // Tu código:
+
+  //
+  // the code :
+  //
 }
 
 
